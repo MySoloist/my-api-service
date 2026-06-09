b010ee327948f6f6ea7070e61c1087f84a1699bd
MySoloist <3540656750@qq.com>
2026-06-09 23:40:44 +0800

    add '//test6' in index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/health', (req, res) => {//test5
+app.get('/health', (req, res) => {//test6
   res.json({ status: 'ok', version: process.env.VERSION || 'dev' });
 });
 
